1853a0e0c5499e695771927d94bf41202de5ae46
TakahashiIkki <new_ittyandesu@outlook.jp>
2018-06-11 00:36:46 +0900

    add http server function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log("Hello! Node.js × flowtype");
+// @flow
+const http = require('http');
+const server = http.createServer();
+let message = 'flowtype';
+
+server.on('request', function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    requestHandler(request, response);
+});
+
+function requestHandler(request, response) {
+    response.end('Hello World & ' + message + ' for HTTP');
+}
+
+server.listen(5001);
